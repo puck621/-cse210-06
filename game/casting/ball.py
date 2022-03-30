73179f48d93018397eda6a853b0b38803eb3142a
puck621 <92011971+puck621@users.noreply.github.com>
2022-03-29 18:25:18 -0600

    Update ball.py

--- game/casting/ball.py
+++ game/casting/ball.py
@@ -5,22 +5,15 @@ from game.casting.point import Point
 
 
 class Ball(Actor):
-    """A solid, spherical object that is bounced around in the game."""
     
     def __init__(self, image):
         """Constructs a new Ball.
-
-        Args:
-            body: A new instance of Body.
-            image: A new instance of Image.
-            
         """
         super().__init__("*",Point(430, 280), Point(0, 0), Point(1, 1))
         # self._body = body
         self._image = image
 
     def bounce_x(self):
-        """Bounces the ball in the x direction."""
         velocity = self._body.get_velocity()
         rn = random.uniform(0.9, 1.1)
         vx = velocity.get_x() * rn * -1
@@ -29,7 +22,6 @@ class Ball(Actor):
         self.set_velocity(velocity)
 
     def bounce_y(self):
-        """Bounces the ball in the y direction."""
         velocity = self._body.get_velocity()
         rn = random.uniform(0.9, 1.1)
         vx = velocity.get_x()
@@ -38,23 +30,12 @@ class Ball(Actor):
         self.set_velocity(velocity)
 
     def get_body(self):
-        """Gets the ball's body.
-        
-        Returns:
-            An instance of Body.
-        """
         return self._body
 
     def get_image(self):
-        """Gets the ball's image.
-        
-        Returns:
-            An instance of Image.
-        """
         return self._image
         
     def release(self):
-        """Release the ball in a random direction."""
         rn = random.uniform(0.9, 1.1)
         vx = random.choice([-BALL_VELOCITY * rn, BALL_VELOCITY * rn])
         vy = -BALL_VELOCITY
