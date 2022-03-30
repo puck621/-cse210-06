747dd7c0a3e1d9823ccd10493fbe2d0d84841009
puck621 <92011971+puck621@users.noreply.github.com>
2022-03-29 18:25:16 -0600

    Update constants.py

--- constants.py
+++ constants.py
@@ -59,13 +59,6 @@ NEXT_LEVEL = 2
 IN_PLAY = 3
 GAME_OVER = 4
 
-# # LEVELS
-# LEVEL_FILE = #"batter/assets/data/level-{:03}.txt"
-# BASE_LEVELS = 5
-
-# -------------------------------------------------------------------------------------------------- 
-# SCRIPTING CONSTANTS
-# -------------------------------------------------------------------------------------------------- 
 
 # PHASES
 INITIALIZE = 0
@@ -76,35 +69,16 @@ OUTPUT = 4
 UNLOAD = 5
 RELEASE = 6
 
-# -------------------------------------------------------------------------------------------------- 
-# CASTING CONSTANTS
-# -------------------------------------------------------------------------------------------------- 
-
-# STATS
-# STATS_GROUP = "stats"
-# DEFAULT_LIVES = 3
-# MAXIMUM_LIVES = 5
-
-# HUD
-# HUD_MARGIN = 15
-# LEVEL_GROUP = "level"
-# LIVES_GROUP = "lives"
-# SCORE_GROUP = "score"
-# LEVEL_FORMAT = "LEVEL: {}"
-# LIVES_FORMAT = "LIVES: {}"
-# SCORE_FORMAT = "SCORE: {}"
 
 # BALL
 BALL_GROUP = "balls"
-BALL_IMAGE = "pong/assets/images/ball.png" #"batter/assets/images/000.png"
 BALL_WIDTH = 28
 BALL_HEIGHT = 28
 BALL_VELOCITY = 6
 
 # PADDLE
 PADDLE_GROUP = "paddles"
-PADDLE_IMAGES = "pong/assets/images/paddle.png" #[f"pong/assets/images/{n:03}.png" for n in range(100, 103)]
-PADDLE_WIDTH = 28
+PADDLE_WIDTH =20
 PADDLE_HEIGHT = 81
 PADDLE_RATE = 6
 PADDLE_VELOCITY = 7
